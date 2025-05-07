b12e7791ca8a5075a32cb2cbd050bf8c4b9553f7
RitaMos <rmoschini@itba.edu.ar>
2025-05-07 12:14:02 -0300

    returning, alfa >= beta, etc
    saque returning porque solo cambiaba su valor con validMoves.empty() y hacia menos legible el codigo
    
    puse if (alfa >= beta) en nodos tanto pares como impares
    
    habia dos ifs que hacian casi lo mismo y me parecio que se podian juntar
    
    mejore comentarios para nuestro uso

--- ai.cpp
+++ ai.cpp
@@ -18,22 +18,21 @@
 
 class TreeNode {
 public:
-	GameModel model;
-	int aiGain;
-	int level;
+    GameModel model;
+    int aiGain;
+    int level;
     std::vector<Square> validMoves;
     int alpha;
     int beta;
     TreeNode* primerHijo;
     bool inProcess;
-    /* un nodo se considera inProcess cuando su vector de validMoves esta siendo estudiado es decir cuando se estan
-       recorriendo sus nodos hijos */
     char currentGain;  //cantidad de fichas volteadas
 
     // Constructor
     TreeNode(GameModel model, char valAlpha, char valBeta, TreeNode* primerHijo, char level, char aiGain, char currentGain, bool inProcess)
-            : model(model), alpha(valAlpha), beta(valBeta), primerHijo(primerHijo), level(level),
-              aiGain(aiGain), currentGain(currentGain), inProcess(inProcess) {} 
+        : model(model), alpha(valAlpha), beta(valBeta), primerHijo(primerHijo), level(level),
+        aiGain(aiGain), currentGain(currentGain), inProcess(inProcess) {
+    }
 
     void agregarHijo(TreeNode* hijo) {
         if (primerHijo == nullptr) {
@@ -51,77 +50,67 @@ Square getBestMove(GameModel& model)
 
     //std::vector<Square> validMoves;
     TreeNode rootNode(model, MINUS_INFINITY, PLUS_INFINITY, nullptr, 1, 0, 0, false);
-    // model, alpha, beta, primerHijo, hermanoSiguiente, level, aiGain, inProcess, currentGain
 
     rootNode.validMoves.clear();
     getValidMoves(model, rootNode.validMoves);
 
     nodeStack.push(&rootNode);
-    Square bestMove = {- 1, -1};
-	Square primordialMove = { -1, -1 }; //primer valid move de la raiz
+    Square bestMove = { -1, -1 };
+    Square primordialMove = { -1, -1 }; //primer valid move de la raiz
 
-	Square move = { -1, -1 }; //VALORES INVALIDOS
+    Square move = { -1, -1 }; //VALORES INVALIDOS
 
-	bool returning = false;
     while (!nodeStack.empty())
     {
-        if (!nodeStack.top()->validMoves.empty()) 
+        nodeStack.top()->inProcess = true;
+        /* un nodo se considera inProcess cuando su vector de validMoves esta siendo estudiado es decir cuando se estan
+       recorriendo sus nodos hijos */
+
+        if (nodeStack.top()->level != MAX_LEVELS && !nodeStack.top()->validMoves.empty())
         {
             move = nodeStack.top()->validMoves.back();
-            /* back porque sacar elementos de la back de un vector es O(1)
-            move = el ultimo validMove del ultimo nodo pusheado
-            */
-            primordialMove = nodeStack.top()->level == 1 ? move : primordialMove;
-            // si el ultimo nodo pusheado es el raiz, entonces primordialMove es el ultimo validMove del nodo raiz
-        }
-        else 
-        {
-			returning = true; // validMoves esta vacio, es decir ya se recorrieron todas las jugadas validas
-        }
+            // move = el ultimo validMove del ultimo nodo pusheado
 
-		nodeStack.top()->inProcess = true;
+            if (nodeStack.top()->level == 1)
+                primordialMove = move;
 
-		if (nodeStack.top()->level != MAX_LEVELS && !returning) //si no hay mas jugadas validas ni entra
-        {
-			nodeStack.top()->agregarHijo(new TreeNode(nodeStack.top()->model, nodeStack.top()->alpha, nodeStack.top()->beta, 
-                                        nullptr, nodeStack.top()->level + 1, 0, 0, false) );
-       
-    	    nodeStack.top()->validMoves.pop_back();
+            nodeStack.top()->agregarHijo(new TreeNode(nodeStack.top()->model, nodeStack.top()->alpha, nodeStack.top()->beta,
+                nullptr, nodeStack.top()->level + 1, 0, 0, false));
+
+            nodeStack.top()->validMoves.pop_back();
             // ya puse el ulimo validMove del ultimo nodo pusheado en "move", ahora lo borro del vector xq ya no lo necesito
 
-			nodeStack.push(nodeStack.top()->primerHijo);
+            nodeStack.push(nodeStack.top()->primerHijo);
 
             nodeStack.top()->currentGain = playMove(nodeStack.top()->model, move) + 1;
             //Mas uno porque cuento las volteadas Y la colocada
 
-			if (!nodeStack.top()->inProcess) // esto no afecta la logica del codigo PERO genera validMoves aunque este en un nodo hoja
-			{
-				nodeStack.top()->validMoves.clear();
-				getValidMoves(nodeStack.top()->model, nodeStack.top()->validMoves);
-			}
+            if (!nodeStack.top()->inProcess) // genera validMoves aunque este en un nodo hoja (no afecta la logica del codigo, solo es algo mas complejo computacionalmente)
+            {
+                nodeStack.top()->validMoves.clear();
+                getValidMoves(nodeStack.top()->model, nodeStack.top()->validMoves);
+            }
         }
-        else if (nodeStack.top()->level == MAX_LEVELS || returning)
+        else if (nodeStack.top()->level == MAX_LEVELS || nodeStack.top()->validMoves.empty())
         {
-			TreeNode* auxNode = nodeStack.top(); //auxNode tiene el hijo
-			nodeStack.pop();
+            TreeNode* auxNode = nodeStack.top(); //auxNode tiene el hijo
+            nodeStack.pop();
             nodeStack.top()->primerHijo = NULL;
-            if (auxNode->level % 2) 
-			{                        //Caso impar. Nodo final (auxNode) es MAX y el padre (nodeStack.top) es MIN
+            if (auxNode->level % 2) // Caso impar
+            {
                 // si el del padre es menor q el del hijo, se lo asigna
                 auxNode->aiGain = -1 * auxNode->currentGain;
                 if (nodeStack.top()->aiGain > auxNode->aiGain)
                 {
                     nodeStack.top()->aiGain = auxNode->aiGain;
-					nodeStack.top()->beta = auxNode->aiGain;
+                    nodeStack.top()->beta = auxNode->aiGain;
                 }
-                
+
                 if (nodeStack.top()->alpha >= nodeStack.top()->beta)
-                {
-                    nodeStack.top()->level = nodeStack.top()->level > 1 ? nodeStack.top()->level = MAX_LEVELS : 1;
-                }
+                    nodeStack.top()->level = (nodeStack.top()->level > 1) ? nodeStack.top()->level = MAX_LEVELS : 1;
             }
-            else
-            {                       //Caso par. Nodo final (auxNode) es MIN y el padre (nodeStack.top) es MAX
+            else // Caso par
+            {
                 auxNode->aiGain = auxNode->currentGain;
                 if (nodeStack.top()->aiGain < auxNode->aiGain)
                 {
@@ -129,27 +118,21 @@ Square getBestMove(GameModel& model)
                     nodeStack.top()->aiGain = auxNode->aiGain;
                     nodeStack.top()->alpha = auxNode->aiGain;
 
-					if (nodeStack.top()->level == 1)
-					{
+                    if (nodeStack.top()->level == 1)
                         bestMove = primordialMove;
-					}
 
-                    if (nodeStack.top()->beta >= nodeStack.top()->alpha)
-                    {
-                        //nodeStack.top()->level = nodeStack.top()->level > 1 ? MAX_LEVELS : 1; //REVISAR
-                        
-                    }
+                    if (nodeStack.top()->alpha >= nodeStack.top()->beta)
+                        nodeStack.top()->level = (nodeStack.top()->level > 1) ? nodeStack.top()->level = MAX_LEVELS : 1;
                 }
             }
             delete auxNode;
-            
+
         }
-         
+
     }
 
 
-    
-	return bestMove;
+    return bestMove;
     // +++ TEST
     // Returns a random valid move...
 /*    Moves validMoves;*/
